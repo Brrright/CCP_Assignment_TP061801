4d65d5524d5ca7dcc3bfd8e6c2a8e42ccf30837f
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-05-25 12:55:26 +0800

    Initial commit

---
+++ src/Model/Customer.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author mingl
+ */
+public class Customer {
+    private int customerID;
+    
+}
