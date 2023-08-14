cbdd1c48d74cc0cb8d5dae47b3e4c5827ce622af
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-08-14 23:35:06 +0800

    Update TinyBusTerminal1.java

--- src/example2/TinyBusTerminal1.java
+++ src/example2/TinyBusTerminal1.java
@@ -87,36 +87,18 @@ public class TinyBusTerminal1 {
         @Override
         public void run() {
             try {
-                // Simulate sometimes the ticket machine isn't working
-                if (new java.util.Random().nextInt(10) > 7) {  // 30% chance
-                    TicketMachine.setWorking(false);
-                } else {
-                    TicketMachine.setWorking(true);
-                }
+                if (terminalQueue.remainingCapacity() > 0) {
+                    terminalQueue.put(this);
+                    buyTicket();
 
-                if (TicketMachine.isWorking()) {
-                    System.out.println("[Customer] Customer " + id + " is buying a ticket from the machine.");
-                    Thread.sleep(500);
-                } else {
-                    System.out.println("[Customer] Customer " + id + " is waiting for the ticket booth.");
-                    ticketBooths.acquire();
-                    System.out.println("[Customer] Customer " + id + " is buying a ticket from the booth.");
-                    Thread.sleep(1000);
-                    ticketBooths.release();
-                }
-
-                if (waitingAreaQueue.remainingCapacity() > 0) {
-                    System.out.println("[Customer] Customer " + id + " moved to the waiting area.");
-                    terminalQueue.remove(this);  // Remove from terminal
-                    waitingAreaQueue.put(this);  // Move to waiting area
-                } else {
-                    System.out.println("[Waiting Area] Waiting area full! Customer " + id + " is waiting in the terminal.");
-                    while (waitingAreaQueue.remainingCapacity() <= 0) {
-                        Thread.sleep(1000);  // wait inside the terminal
+                    int waitingAreaIndex = new Random().nextInt(3);
+                    if (waitingAreas[waitingAreaIndex].offer(this)) {
+                        System.out.println("[Customer] Customer " + id + " moved to waiting area " + (char) ('A' + waitingAreaIndex));
+                    } else {
+                        System.out.println("[Waiting Area] Waiting area " + (char) ('A' + waitingAreaIndex) + " is full! Customer " + id + " is waiting in the terminal.");
+                        waitingAreas[waitingAreaIndex].put(this);
                     }
-                    waitingAreaQueue.put(this);
                 }
-
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
@@ -141,15 +123,17 @@ public class TinyBusTerminal1 {
         public void run() {
             try {
                 while (true) {
-                    // Iterate through each minibus to inspect tickets
-                    for (BlockingQueue<Minibus> minibusQueue : minibusQueues) {
-                        if (!minibusQueue.isEmpty()) {
-                            Minibus minibus = minibusQueue.take();
-                            minibus.checkTickets();
-                            minibusQueue.offer(minibus);
+                    for (BlockingQueue<Customer> waitingArea : waitingAreas) {
+                        if (!waitingArea.isEmpty()) {
+                            Customer customer = waitingArea.poll();
+                            if (customer.hasTicket) {
+                                System.out.println("[Inspector] Customer " + customer.id + " has a valid ticket and can board the minibus.");
+                            } else {
+                                System.out.println("[Inspector] Customer " + customer.id + " was denied boarding due to not having a ticket.");
+                            }
                         }
+                        Thread.sleep(1000); // sleep for a while before checking the next waiting area
                     }
-                    Thread.sleep(1000); // sleep for a while before checking again
                 }
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
@@ -167,42 +151,36 @@ public class TinyBusTerminal1 {
         }
 
         public void checkTickets() throws InterruptedException {
+            Queue<Customer> allowedCustomers = new LinkedList<>();
             while (!onboardingQueue.isEmpty()) {
                 Customer customer = onboardingQueue.poll();
                 System.out.println("[Inspector] Inspector is now checking ticket of Customer " + customer.id);
                 if (customer.hasTicket) {
                     System.out.println("[Customer] Customer " + customer.id + " has a valid ticket.");
+                    allowedCustomers.add(customer);
                 } else {
                     System.out.println("[Customer] Customer " + customer.id + " was denied boarding due to not having a ticket.");
                 }
             }
+            onboardingQueue.addAll(allowedCustomers);
         }
 
         @Override // run of Terminal
         public void run() {
             try {
+                int waitingAreaIndex = new Random().nextInt(3); // Randomly choose one of the three waiting areas (A, B, or C)
                 while (true) {
-                    if (waitingAreaQueue.size() < 10) {
-//                        int numLeft = 10 - waitingAreaQueue.size();
-//                        System.out.println("[Mini Bus] Minibus waiting for " + numLeft + " customers...");
+                    if (waitingAreas[waitingAreaIndex].size() < 10) {
                         Thread.sleep(MINIBUS_CHECK_INTERVAL_MS);
                     } else {
-                        for (int i = 0; i < 10 && !waitingAreaQueue.isEmpty(); i++) {
-                            Customer customer = waitingAreaQueue.take();
+                        for (int i = 0; i < 10 && !waitingAreas[waitingAreaIndex].isEmpty(); i++) {
+                            Customer customer = waitingAreas[waitingAreaIndex].take();
                             onboardingQueue.offer(customer);
-                            System.out.println("[Customer] Customer " + customer.id + " has queued up to enter the bus.");
-                            //Ticket checking as customer boards minibus
-                            if (customer.hasTicket) {
-                                System.out.println("[Inspector] Customer " + customer.id + " has a valid ticket.");
-                            } else {
-                                System.out.println("[Inspector] Customer " + customer.id + " was denied boarding due to not having a ticket.");
-                                onboardingQueue.remove(customer);
-                            }
+                            System.out.println("[Customer] Customer " + customer.id + " has queued up to enter the bus from waiting area " + (char) ('A' + waitingAreaIndex) + ".");
                         }
-
-                        System.out.println("[Mini Bus] Minibus departing...");
+                        System.out.println("[Mini Bus] Minibus departing from waiting area " + (char) ('A' + waitingAreaIndex) + "...");
                         Thread.sleep(MINIBUS_TRAVEL_TIME_MS);
-                        System.out.println("[Mini Bus] Minibus returned to the terminal.");
+                        System.out.println("[Mini Bus] Minibus returned to the terminal from waiting area " + (char) ('A' + waitingAreaIndex) + ".");
                         Thread.sleep(20000);
                     }
                 }
@@ -258,76 +236,97 @@ public class TinyBusTerminal1 {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService customerExecutor = Executors.newFixedThreadPool(80);
-        ExecutorService minibusExecutor = Executors.newSingleThreadExecutor();
-
-
-        // <editor-fold>
-//         Occasionally simulate booth staff on break
-        ScheduledExecutorService staffBreaks = Executors.newScheduledThreadPool(1);
+        ExecutorService customerExecutor = Executors.newFixedThreadPool(MAX_CUSTOMERS);
         ScheduledExecutorService machineBreaks = Executors.newScheduledThreadPool(1);
         occasionallyBreakTicketMachine(machineBreaks);
 
-        staffBreaks.scheduleAtFixedRate(() -> {
-            if (ticketBooths.availablePermits() > 0) {
-                try {
-                    ticketBooths.acquire(); // simulate staff going on break
-                    System.out.println("One ticket booth staff is on a break!");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 10, 30, TimeUnit.SECONDS);
+        // Initiate ticket inspector thread
+        new Thread(new TicketInspector()).start();
 
-        staffBreaks.scheduleAtFixedRate(() -> {
-            if (ticketBooths.availablePermits() < 2) {
-                ticketBooths.release(); // staff back from break
-                System.out.println("Ticket booth staff is back from break!");
-            }
-        }, 15, 30, TimeUnit.SECONDS);
-// </editor-fold>
-        //        TinyBusTerminal1 terminal = new TinyBusTerminal1();
-        // Initialize and start the minibuses
+        // Create and start minibuses
         for (int i = 0; i < NUMBER_OF_MINIBUSES; i++) {
-            Minibus minibus = new Minibus(minibusQueues[i]);
-            minibusQueues[i].offer(minibus);
-            new Thread(minibus).start();
+            new Thread(new Minibus(minibusQueues[i])).start();
         }
-        TicketInspector ticketInspector = new TicketInspector();
-        new Thread(ticketInspector).start();
-
-        ScheduledExecutorService entranceExecutor = Executors.newScheduledThreadPool(2);
-        handleEntrances(entranceExecutor);
 
         // Simulate customer arrivals
         for (int i = 1; i <= MAX_CUSTOMERS; i++) {
             Customer c = new Customer(i);
-            if (new java.util.Random().nextBoolean()) {
-                new Thread(() -> {
-                    try {
-                        c.enterTerminalFromEntrance(WEST_ENTRANCE);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-            } else {
-                new Thread(() -> {
-                    try {
-                        c.enterTerminalFromEntrance(EAST_ENTRANCE);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-            }
+            customerExecutor.execute(c);
             Thread.sleep(new java.util.Random().nextInt(3) * 1000 + 1000);
         }
 
         customerExecutor.shutdown();
-        customerExecutor.awaitTermination(1, TimeUnit.HOURS);  // or an appropriate duration
-        minibusExecutor.shutdown();
-        minibusExecutor.awaitTermination(1, TimeUnit.HOURS);
-        entranceExecutor.shutdown();
-//        staffBreaks.shutdown();
-//        machineBreaks.shutdown();
+        customerExecutor.awaitTermination(1, TimeUnit.HOURS);
+        machineBreaks.shutdown();
+//        ExecutorService customerExecutor = Executors.newFixedThreadPool(80);
+//        ExecutorService minibusExecutor = Executors.newSingleThreadExecutor();
+//
+//        // <editor-fold>
+////         Occasionally simulate booth staff on break
+//        ScheduledExecutorService staffBreaks = Executors.newScheduledThreadPool(1);
+//        ScheduledExecutorService machineBreaks = Executors.newScheduledThreadPool(1);
+//        occasionallyBreakTicketMachine(machineBreaks);
+//
+//        staffBreaks.scheduleAtFixedRate(() -> {
+//            if (ticketBooths.availablePermits() > 0) {
+//                try {
+//                    ticketBooths.acquire(); // simulate staff going on break
+//                    System.out.println("One ticket booth staff is on a break!");
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }, 10, 30, TimeUnit.SECONDS);
+//
+//        staffBreaks.scheduleAtFixedRate(() -> {
+//            if (ticketBooths.availablePermits() < 2) {
+//                ticketBooths.release(); // staff back from break
+//                System.out.println("Ticket booth staff is back from break!");
+//            }
+//        }, 15, 30, TimeUnit.SECONDS);
+//// </editor-fold>
+//        //        TinyBusTerminal1 terminal = new TinyBusTerminal1();
+//        // Initialize and start the minibuses
+//        for (int i = 0; i < NUMBER_OF_MINIBUSES; i++) {
+//            Minibus minibus = new Minibus(minibusQueues[i]);
+//            minibusQueues[i].offer(minibus);
+//            new Thread(minibus).start();
+//        }
+//        TicketInspector ticketInspector = new TicketInspector();
+//        new Thread(ticketInspector).start();
+//
+//        ScheduledExecutorService entranceExecutor = Executors.newScheduledThreadPool(2);
+//        handleEntrances(entranceExecutor);
+//
+//        // Simulate customer arrivals
+//        for (int i = 1; i <= MAX_CUSTOMERS; i++) {
+//            Customer c = new Customer(i);
+//            if (new java.util.Random().nextBoolean()) {
+//                new Thread(() -> {
+//                    try {
+//                        c.enterTerminalFromEntrance(WEST_ENTRANCE);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }).start();
+//            } else {
+//                new Thread(() -> {
+//                    try {
+//                        c.enterTerminalFromEntrance(EAST_ENTRANCE);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }).start();
+//            }
+//            Thread.sleep(new java.util.Random().nextInt(3) * 1000 + 1000);
+//        }
+//
+//        customerExecutor.shutdown();
+//        customerExecutor.awaitTermination(1, TimeUnit.HOURS);  // or an appropriate duration
+//        minibusExecutor.shutdown();
+//        minibusExecutor.awaitTermination(1, TimeUnit.HOURS);
+//        entranceExecutor.shutdown();
+////        staffBreaks.shutdown();
+////        machineBreaks.shutdown();
     }
 }
